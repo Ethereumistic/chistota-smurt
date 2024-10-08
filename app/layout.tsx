ecef4079bb32e048841360554629e9afc729f675
Ethereumistic <badjarovv@gmail.com>
2024-10-08 11:51:37 +0300

    pixel 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -109,7 +109,7 @@ export default function RootLayout({
           height="1"
           width="1"
           style={{ display: "none" }}
-          src={`https://www.facebook.com/tr?id=YOUR_PIXEL_ID&ev=PageView&noscript=1`}
+          src={`https://www.facebook.com/tr?id=1969169503524104&ev=PageView&noscript=1`}
         />
       </noscript>
 
