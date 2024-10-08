5dbe0b105456a92f549710da470008dbf5edb659
Ethereumistic <badjarovv@gmail.com>
2024-10-08 11:41:10 +0300

    pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ import Footer from "./components/ui/Footer";
 import { Russo_One } from "next/font/google";
 import { Montserrat } from "next/font/google";
 import { NavbarTest } from "./components/NavbarTest";
+import Script from "next/script";
+
+
 
 const montserrat = Montserrat({
   subsets: ['latin', 'cyrillic'],
@@ -83,7 +86,33 @@ export default function RootLayout({
             <DotEffect dotSize={1} spacing={20}  />
             <Footer />
           </div>
-        </Providers>
+          </Providers>
+          <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+      >
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1969169503524104');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=YOUR_PIXEL_ID&ev=PageView&noscript=1`}
+        />
+      </noscript>
+
       </body>
       </html>
   );
