5a9ec2daf8369d2705bd4d085c58d16445a5afad
Ethereumistic <badjarovv@gmail.com>
2024-10-23 15:07:40 +0300

    resposniveness 2 no tracker 3

--- app/components/cover/MobileCoverT.tsx
+++ app/components/cover/MobileCoverT.tsx
@@ -8,21 +8,21 @@ export function MobileCoverT() {
 
   return (
     <div>
-    <div className="relative h-screen overflow-hidden"> {/* Full height for mobile/tablet */}
+    <div className="relative h-screen overflow-hidden">
       <Image 
         src={bg} 
         alt="Background" 
-        layout="fill" // Fill the parent container
-        objectFit="cover" // Cover the entire area
-        className="z-0" // Background image behind other elements
+        layout="fill"
+        objectFit="cover"
+        className="z-0"
       />
       <Image 
         src={overlayImage} 
         alt="Overlay" 
-        layout="intrinsic" // Use intrinsic layout for responsive sizing
-        width={350} // Set width to half of the background image width (adjust as needed)
-        height={100} // Set height to half of the background image height (adjust as needed)
-        className="absolute bottom-0 left-1/2 transform -translate-x-1/2 z-10 " // Center the overlay image
+        layout="intrinsic"
+        width={1080} 
+        height={100} 
+        className="absolute bottom-0 left-1/2 transform -translate-x-1/2 z-10 overlay-image" 
       />
     </div>
     
