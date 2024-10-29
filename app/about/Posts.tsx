a0161b338ec7157d5f0dbb2037af3a302b8f5bbc
Ethereumistic <badjarovv@gmail.com>
2024-10-29 14:22:30 +0200

    blog new rendering implement

--- app/about/Posts.tsx
+++ app/about/Posts.tsx
@@ -11,7 +11,15 @@ const Posts = () => {
   useEffect(() => {
     const fetchPosts = async () => {
       const fetchedPosts = await getPosts();
-      setPosts(fetchedPosts);
+      
+      // Sort posts by publishedAt date in descending order
+      const sortedPosts = fetchedPosts.sort((a, b) => {
+        const dateA = new Date(a.publishedAt); // Parse publishedAt to Date
+        const dateB = new Date(b.publishedAt); // Parse publishedAt to Date
+        return dateB.getTime() - dateA.getTime(); // Compare timestamps
+      });
+
+      setPosts(sortedPosts);
       setLoading(false);
     };
 
@@ -23,29 +31,58 @@ const Posts = () => {
   }
 
   return (
-    <div className="flex flex-wrap justify-center max-w-[90%] sm:max-w-6xl bg-transparent mx-4 rounded-xl">
-      {posts.map((post) => (
-        <Link
-          href={`/about/${post.slug.current}`}
-          key={post._id}
-          className="flex flex-col items-center overflow-hidden transition-all duration-300 w-full sm:w-1/2 lg:w-1/3 p-4"
-        >
-          <div className="flex flex-col justify-between h-full w-full p-6">
-            {post.mainImage && (
-              <div className="relative w-full h-64 mb-1 rounded-xl overflow-hidden">
-                <Image
-                  src={post.mainImage.asset.url}
-                  alt={post.mainImage.alt || post.title}
-                  layout="fill"
-                  objectFit="cover"
-                  className="rounded-xl"
-                />
+    <div className="w-full mx-auto pb-4 flex justify-center items-center">
+      {/* Flex Layout */}
+      <div className="flex flex-wrap justify-center max-w-[90%] sm:max-w-6xl bg-transparent mx-4 rounded-xl">
+        {posts.map((post) => (
+          <Link
+            href={`/about/${post.slug.current}`}
+            key={post._id}
+            className="flex flex-col items-center overflow-hidden transition-all duration-300 w-full sm:w-1/2 lg:w-1/3 p-4"
+          >
+            {/* Content */}
+            <div className="flex flex-col justify-between h-full w-full p-6">
+              {/* Image */}
+              {post.mainImage && (
+                <div className="relative w-full h-64 mb-1 rounded-xl overflow-hidden">
+                  <Image
+                    src={post.mainImage.asset.url}
+                    alt={post.mainImage.alt || post.title}
+                    layout="fill" // Use layout fill to cover the parent div
+                    objectFit="cover" // Maintain aspect ratio
+                    className="rounded-xl" // Keep rounded corners
+                  />
+                </div>
+              )}
+
+              {/* Meta Information */}
+              <div className="text-gray-900 text-sm flex justify-end items-center space-x-2 my-2">
+                <span>{post.author.name || 'Author'}</span>
+                <span>&bull;</span>
+                <span>{new Date(post.publishedAt).toLocaleDateString()}</span>
               </div>
-            )}
-            <h2 className="text-2xl font-semibold mb-4 text-black flex-1">{post.title}</h2>
-          </div>
-        </Link>
-      ))}
+
+              {/* Categories - Render all categories if there are multiple */}
+              <div className="mb-2">
+                {post.categories.length > 1 ? (
+                  <ul className="flex justify-between text-purple-600 uppercase text-xs">
+                    {post.categories.map((category: any) => (
+                      <li key={category._id}>{category.title}</li>
+                    ))}
+                  </ul>
+                ) : (
+                  <span className="text-purple-600 uppercase text-xs">
+                    {post.categories[0]?.title || 'Category'}
+                  </span>
+                )}
+              </div>
+
+              {/* Title */}
+              <h2 className="text-2xl font-semibold mb-4 text-black flex-1">{post.title}</h2>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,44 +1,36 @@
-import Image from 'next/image'
-import { getPosts } from './getPosts'
-import Link from 'next/link'
+'use client'
 import AboutUs from './AboutUs';
 import dynamic from 'next/dynamic'
 import AboutMov from './AboutMov';
 import Posts from './Posts';
-// import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 const ReactLenis = dynamic(() => import('lenis/react').then((mod) => mod.ReactLenis), {
   ssr: false
 })
-export default async function About() {
-  const posts = await getPosts()
+export default function About() {
 
-   // Sort posts by publishedAt date in descending order
-   const sortedPosts = posts.sort((a, b) => {
-    const dateA = new Date(a.publishedAt); // Parse publishedAt to Date
-    const dateB = new Date(b.publishedAt); // Parse publishedAt to Date
-    return dateB.getTime() - dateA.getTime(); // Compare timestamps
-  });
+
    
-  // const blogRef = useRef<HTMLHeadingElement | null>(null);
+  const blogRef = useRef<HTMLHeadingElement | null>(null);
 
-  // useEffect(() => {
-  //   const hash = window.location.hash;
-  //   if (hash === '#blog' && blogRef.current) {
-  //     // Scroll to the element
-  //     blogRef.current.scrollIntoView({ behavior: 'smooth' });
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash === '#blog' && blogRef.current) {
+      // Scroll to the element
+      blogRef.current.scrollIntoView({ behavior: 'smooth' });
 
-  //     // Adjust the scroll position by a specific number of pixels
-  //     const offset = 120; // Adjust this value as needed
-  //     const elementPosition = blogRef.current.getBoundingClientRect().top + window.scrollY;
-  //     const offsetPosition = elementPosition - offset;
+      // Adjust the scroll position by a specific number of pixels
+      const offset = 120; // Adjust this value as needed
+      const elementPosition = blogRef.current.getBoundingClientRect().top + window.scrollY;
+      const offsetPosition = elementPosition - offset;
 
-  //     window.scrollTo({
-  //       top: offsetPosition,
-  //       behavior: 'smooth'
-  //     });
-  //   }
-  // }, []);
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+    }
+  }, []);
 
 
   return (  
@@ -51,67 +43,11 @@ export default async function About() {
     <div className='mt-8 justify-center items-center'>
       <AboutMov />
       <AboutUs />
-      <h1 id="blog"  className="text-5xl px-4 text-black uppercase font-black text-center mb-12">Блог</h1>
+      <h1 id="blog" ref={blogRef} className="text-5xl px-4 text-black uppercase font-black text-center mb-12">Блог</h1>
       <div className='flex justify-center items-center'>
       <Posts />
       </div>
-      <div className="w-full mx-auto  pb-4 flex justify-center items-center">
 
-        {/* Flex Layout */}
-        <div className="flex flex-wrap justify-center max-w-[90%] sm:max-w-6xl bg-transparent mx-4 rounded-xl">
-        {sortedPosts.map((post) => (
-            <Link
-              href={`/about/${post.slug.current}`}
-              key={post._id}
-              className="flex flex-col items-center overflow-hidden transition-all duration-300 w-full sm:w-1/2 lg:w-1/3 p-4"
-            >
-              {/* Content */}
-              <div className="flex flex-col justify-between h-full w-full  p-6 ">
-
-                            {/* Image */}
-                            {post.mainImage && (
-              <div className="relative w-full h-64 mb-1 rounded-xl overflow-hidden"> {/* Set a fixed height for the square */}
-                <Image
-                  src={post.mainImage.asset.url}
-                  alt={post.mainImage.alt || post.title}
-                  layout="fill" // Use layout fill to cover the parent div
-                  objectFit="cover" // Maintain aspect ratio
-                  className="rounded-xl" // Keep rounded corners
-                />
-              </div>
-            )}
-
-                  {/* Meta Information */}
-                  <div className="text-gray-900 text-sm flex justify-end items-center space-x-2 my-2">
-                  <span>{post.author.name || 'Author'}</span>
-                  <span>&bull;</span>
-                  <span>{new Date(post.publishedAt).toLocaleDateString()}</span>
-                </div>
-
-                {/* Categories - Render all categories if there are multiple */}
-                <div className="mb-2">
-                  {post.categories.length > 1 ? (
-                    <ul className="flex justify-between text-purple-600 uppercase text-xs">
-                      {post.categories.map((category) => (
-                        <li key={category._id}>{category.title}</li>
-                      ))}
-                    </ul>
-                  ) : (
-                    <span className="text-purple-600 uppercase text-xs">
-                      {post.categories[0]?.title || 'Category'}
-                    </span>
-                  )}
-                </div>
-
-                {/* Title */}
-                <h2 className="text-2xl font-semibold mb-4 text-black flex-1">{post.title}</h2>
-
-
-              </div>
-            </Link>
-          ))}
-        </div>
-      </div>
     </div>
     </ReactLenis>
   )
